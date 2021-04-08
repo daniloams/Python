e96c75b745d031805474cc9a9ff030f15c3b91d5
daniloams <danilo.andrademartins@ufpe.br>
2021-04-08 10:50:24 -0300

    testinhos
    práticas relativas a aula 03 do curso

---
+++ desafio002.py
@@ -0,0 +1,6 @@
+nome = input('Oi, qual é o seu nome?')
+dia = input('Qual o dia que você nasceu?')
+mes = input('QUal foi o mês?')
+ano = input('E de qual ano?')
+
+print('Oi, seu nome é?', nome, 'você nasceu em:', dia, mes, ano)
---
+++ desafio003.py
@@ -0,0 +1,4 @@
+n1 = input('Qual é o teu numero?')
+n2 = input('E qual é o teu?')
+
+print('a soma é:', n1+n2)
